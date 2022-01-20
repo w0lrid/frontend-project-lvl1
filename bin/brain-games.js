d4031f62dc16b81600b1b0c172778e65146dfd7d
Dmitrii Kushnerev <w0lridem@gmail.com>
2022-01-21 01:08:21 +0300

    feat: set environment

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { greet as greetPlayer } from "../src/cli.js";
+import greet from "../src/cli.js";
 
-greetPlayer();
+greet();
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
 import readlineSync from "readline-sync";
 
-export const greet = () => {
-    console.log("Welcome to the Brain Games!");
+const greet = () => {
+  console.log("Welcome to the Brain Games!");
 
-    setTimeout(() => {
-        const name = readlineSync.question("What's your name, hero? ");
-        console.log(`Hello, ${name}`);
-    }, 500);
+  setTimeout(() => {
+    const name = readlineSync.question("What's your name, hero? ");
+    console.log(`Hello, ${name}`);
+  }, 500);
 };
+
+export default greet;
