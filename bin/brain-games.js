c0e7b194dcefd7bf1c5a7af19bf24da786b874c1
Dmitrii Kushnerev <w0lridem@gmail.com>
2022-02-06 01:28:40 +0300

    fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-console.log('/May I have your name?/');
+// console.log('/May I have your name?/');
 console.log('Welcome to the Brain Games!');
 
 const name = readlineSync.question("What's your name, hero? ");
