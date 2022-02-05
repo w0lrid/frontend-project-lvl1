d4932f2580608ff37b241459f3209c08b555d4b2
Dmitrii Kushnerev <w0lridem@gmail.com>
2022-02-06 00:28:35 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,15 @@
+import readlineSync from "readline-sync";
+
+export const greet = () => {
+  console.log("Welcome to the Brain Games!");
+
+  const name = readlineSync.question("What's your name, hero? ");
+
+  console.log(`Hello, ${name}`);
+
+  return name;
+};
+
 export const generateRandomNumber = (maxBorder) => Math.floor(Math.random() * maxBorder);
 
 export const generateRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
